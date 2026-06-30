fcf65cd6c22dd745c5c985e98ba3285cfc7c6e2d
Matthew Boniface <mboniface5@yahoo.com>
2026-06-30 15:40:13 +0100

    Create the main file to run from and add the mag 7 as the initial tickers to test

---
+++ main.py
@@ -0,0 +1,7 @@
+def run_optimisation_engine(tickers):
+    return
+    
+
+if __name__ == 'main':
+    tickers = ["AAPL","NVDA","MSFT","GOOGL","AMZN","META","TSLA"]
+    run_optimisation_engine(tickers)
